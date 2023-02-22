256a7b75724089b02b3864dbe1b2d9536a71105f
Everly <everly.bcc011@gmail.com>
2023-02-22 15:46:18 -0300

    Pesquisar por tipo e habilidade

--- app/src/main/java/com/example/pokedex/PesquisarHabilidade.java
+++ app/src/main/java/com/example/pokedex/PesquisarHabilidade.java
@@ -1,8 +1,11 @@
 package com.example.pokedex;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,7 +14,23 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class PesquisarHabilidade extends AppCompatActivity {
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    ListView lvHabilidade;
+    private String url = "http://192.168.15.5/pokedexApi/public/api/pesquisahabilidade";
+    private String url2;
 
     private Spinner habilidadeSpinner;
     private ArrayAdapter<CharSequence> habilidadeAdapter;
@@ -24,7 +43,7 @@ public class PesquisarHabilidade extends AppCompatActivity {
         habilidadeSpinner = findViewById(R.id.spinnerHabilidade);
         Button submitButton;
         submitButton = findViewById(R.id.botaoPesquisar);
-        ListView lvHabilidade = findViewById(R.id.listViewPesquisar);
+        lvHabilidade = findViewById(R.id.listViewPesquisar);
 
         //Cria ArrayAdapter usando um string array e um spinnerlayout
         habilidadeAdapter = ArrayAdapter.createFromResource(this, R.array.habilidade_poke, R.layout.spinner_layout);
@@ -53,9 +72,45 @@ public class PesquisarHabilidade extends AppCompatActivity {
             } else {
                 //Caso tudo ok  PENDENTE
                 String tipoString = habilidadeSpinner.getSelectedItem().toString();
-                Toast.makeText(PesquisarHabilidade.this, "Pesquisa ok", Toast.LENGTH_LONG).show();
+//                Toast.makeText(PesquisarHabilidade.this, "Pesquisa ok", Toast.LENGTH_LONG).show();
+                url2 = url+"/?habilidade="+tipoString;
+                getDataHab();
+                Log.i(TAG, "Url :" + url2);
             }
         });
+    }
+
+    private void getDataHab() {
+        mRequestQueue = Volley.newRequestQueue(this);
+        mStringRequest = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+                try {
+                    JSONObject data = new JSONObject(response);
+                    JSONArray subjects = data.getJSONArray("pokemons");
+                    String[] subjectsArray = new String[subjects.length()];
+                    for(int i = 0; i < subjects.length(); i++){
+                        subjectsArray[i] = subjects.getString(i);
+                    }
+                    ArrayAdapter<String> array = new ArrayAdapter<String>(
+                            getApplicationContext(),
+                            android.R.layout.simple_list_item_1,
+                            subjectsArray);
+                    lvHabilidade.setAdapter(array);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error :" + error.toString());
+                Toast.makeText(getApplicationContext(), "Erro de conexão com a api.", Toast.LENGTH_LONG).show();
+            }
+        });
+        mRequestQueue.add(mStringRequest);
     }
 
 }
--- app/src/main/java/com/example/pokedex/PesquisarTipo.java
+++ app/src/main/java/com/example/pokedex/PesquisarTipo.java
@@ -1,8 +1,11 @@
 package com.example.pokedex;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,7 +14,23 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class PesquisarTipo extends AppCompatActivity {
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    ListView lvTipo;
+    private String url = "http://192.168.15.5/pokedexApi/public/api/pesquisatipo";
+    private String url2;
 
     private Spinner tipoSpinner;
     private ArrayAdapter<CharSequence> tipoAdapter;
@@ -25,7 +44,7 @@ public class PesquisarTipo extends AppCompatActivity {
         tipoSpinner = findViewById(R.id.spinnerTipo);
         Button submitButton;
         submitButton = findViewById(R.id.botaoPesquisar);
-        ListView lvTipo = findViewById(R.id.listViewTipo);
+        lvTipo = findViewById(R.id.listViewTipo);
 
         //Cria ArrayAdapter usando um string array e um spinnerlayout
         tipoAdapter = ArrayAdapter.createFromResource(this, R.array.tipo_poke, R.layout.spinner_layout);
@@ -54,8 +73,44 @@ public class PesquisarTipo extends AppCompatActivity {
             } else {
                 //Caso tudo ok  PENDENTE
                 String tipoString = tipoSpinner.getSelectedItem().toString();
-                Toast.makeText(PesquisarTipo.this, "Pesquisa ok", Toast.LENGTH_LONG).show();
+//                Toast.makeText(PesquisarTipo.this, "Pesquisa ok", Toast.LENGTH_LONG).show();
+                url2 = url+"/?tipo="+tipoString;
+                Log.i(TAG, "Url :" + url2);
+                getDataTipo();
             }
         });
+    }
+
+    private void getDataTipo() {
+        mRequestQueue = Volley.newRequestQueue(this);
+        mStringRequest = new StringRequest(Request.Method.GET, url2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                Toast.makeText(getApplicationContext(), "Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+                try {
+                    JSONObject data = new JSONObject(response);
+                    JSONArray subjects = data.getJSONArray("nomes");
+                    String[] subjectsArray = new String[subjects.length()];
+                    for(int i = 0; i < subjects.length(); i++){
+                        subjectsArray[i] = subjects.getString(i);
+                    }
+                    ArrayAdapter<String> array = new ArrayAdapter<String>(
+                            getApplicationContext(),
+                            android.R.layout.simple_list_item_1,
+                            subjectsArray);
+                    lvTipo.setAdapter(array);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error :" + error.toString());
+                Toast.makeText(getApplicationContext(), "Erro de conexão com a api.", Toast.LENGTH_LONG).show();
+            }
+        });
+        mRequestQueue.add(mStringRequest);
     }
 }
